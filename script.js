1192645ef7d190689d23e9ea47dd3aae38518e9c
Vikash-shakya495 <vikashshakya367@gmail.com>
2024-05-25 11:20:39 +0530

    added some js conconcept through version control system

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('input[type="submit"]');
+button.addEventListener('click', function() {
+    var form = document.querySelector('form');
+    form.style.backgroundColor = "yellow";
+    alert("welcome to Gla University");
+});
